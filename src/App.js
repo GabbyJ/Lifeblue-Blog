df92bb227575b6573b3c832d6832c782229fdda2
Gabrielle Jacobs <GabbyJ424@gmail.com>
2021-01-26 23:08:27 -0600

    create blog card

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ function App() {
     <div className="container">
       <header>
         <h1>Company Blog</h1>
+        <div className="row">
+          <div className="left-column">
+            <div className="card">
+              <h2>TITLE HEADING</h2>
+              <h5>Title description, Jan, 24, 2021</h5>
+              <img src="https://via.placeholder.com/350" alt="blog placeholder"></img>
+              <p>Some text...</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+          </div>
+          <div className="right-column">
+            <div className="card">
+              
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
